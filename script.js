992c09e6fd3a0a518aad2d6e04217a86a9a26e28
Atilla Bora Semerci <59848942+etzellux@users.noreply.github.com>
2022-03-31 11:22:01 +0300

    update script

--- script.js
+++ script.js
@@ -1,22 +1,14 @@
-function onClick(gifCount)
+function onClick()
 {
     const parent_div = document.getElementById("mybutton").parentElement;
-    if(gifCount%2 == 0)
-    {
-        const myimg = document.createElement("img");
-        myimg.src = "https://c.tenor.com/ckOaiBGgA4EAAAAd/love-it-love.gif";
-        myimg.id = "myimg"
-        parent_div.appendChild(myimg);
-    }
-    else
-    {
-        parent_div.removeChild(document.getElementById("myimg"));
-    }
-    gifCount += 1;
+    const myimg = document.createElement("img");
+    myimg.src = "https://c.tenor.com/ckOaiBGgA4EAAAAd/love-it-love.gif";
+    myimg.id = "myimg"
+    parent_div.appendChild(myimg);
 }
 
 var gifCount = 0;
 const button = document.getElementById("mybutton");
-button.addEventListener("click", onClick(gifCount));
+button.addEventListener("click", onClick);
 
 
